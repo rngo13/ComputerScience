224cf629829bef27a8d97d3a4a67ef41c6a6e591
rngo13 <ryanngo2007@gmail.com>
2024-09-25 08:50:11 -0500

    animal survey

--- Assignments/AnimalSurvey.py
+++ Assignments/AnimalSurvey.py
@@ -1,3 +1,13 @@
-
-
-
+user_input1 = input("What is your favorite animal?\n")
+print(">" + user_input1)
+user_input2 = input("Why do you like them?\n")
+print(">" + user_input2)
+user_input3 = input("What sound do they make?\n")
+print(">" + user_input3)
+user_input4 = input("What color are they?\n")
+print(">" + user_input4)
+user_input5 = input("What do they eat?\n")
+print(">" + user_input5)
+user_input6 = input("Where do they live?\n")
+print(">" + user_input6)
+print("Your favorite animal is a " + user_input1 + " because " + user_input2 + "." + " They make a noise like " + user_input3 + " and they are " + user_input4 + "." + " They eat " + user_input5 + " and they live in " + user_input6 + ".")
--- Unit 2 - Functions and Variables/practice_sept-20.py
+++ Unit 2 - Functions and Variables/practice_sept-20.py
@@ -3,3 +3,6 @@
 
 
 
+
+user_input = input("Enter a Number\n")
+print(">" + user_input)
