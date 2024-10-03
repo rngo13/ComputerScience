a4374befa5b689f3db8cb2f141cffeb9aeb7c454
rngo13 <ryanngo2007@gmail.com>
2024-10-03 09:40:22 -0500

    Unit2 TEST

--- Tests/unit2test.py
+++ Tests/unit2test.py
@@ -7,14 +7,14 @@ print(word_1 + " " + word_2 + " " + word_3)
 
 
 
-user_1 = input("type in an integer\n>")
-user_2 = input("type in another integer\n>")
-user_3 = input("type in your last integer\n>")
+user_1 = int(input("type in an integer\n>"))
+user_2 = int(input("type in another integer\n>"))
+user_3 = int(input("type in your last integer\n>"))
 def add_three(x,y,z):
     print(x + y + z)
 
 
-add_three(4,5,7)
+add_three(user_1,user_2,user_3)
 
 
 
@@ -22,7 +22,7 @@ add_three(4,5,7)
 def data_three():
     word = input("type in a word\n>")
     integer = input("type in an integer\n>")
-    float = float(input("type in a float\n>"))
+    float = (input("type in a float\n>"))
     print(integer + float + " and " + word)
 
 data_three()
---
+++ Unit 2 - Functions and Variables/random2.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+def data_three():
+    word = input("type in a word\n>")
+    integer = (input("type in an integer\n>"))
+    float_1 = (input("type in a float\n>"))
+    print((integer + float_1) + " and " + word)
+
+data_three()24
